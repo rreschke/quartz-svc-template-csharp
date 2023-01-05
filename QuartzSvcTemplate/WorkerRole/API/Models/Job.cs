9c8512d5c6e269d18193d8af33c65d706d28a090
Renan Reschke <renanreschke@hotmail.com>
2023-01-05 20:03:14 -0300

    API para executar jobs remotamente

---
+++ QuartzSvcTemplate/WorkerRole/API/Models/Job.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using CrystalQuartz.Core.Utils;
+using Quartz;
+using Quartz.Impl.Matchers;
+
+namespace WorkerRole.API.Models
+{
+    [DataContract(Namespace = "", Name = "Job")]
+    public class Job
+    {
+        private static IScheduler _scheduler = WorkerRole.Instance;
+
+        [DataMember]
+        public string Group { get; set; }
+
+        [DataMember]
+        public string Name { get; set; }
+
+        public static IEnumerable<Job> GetJobs()
+        {
+            return _scheduler.GetJobKeys(GroupMatcher<JobKey>.AnyGroup()).Result
+                             .Select(jobKey => _scheduler.GetJobDetail(jobKey).Result.Key)
+                             .Select(key => new Job { Group = key.Group, Name = key.Name}); 
+        }
+
+        public static long GetLastRunTime(string jobGroup, string jobName)
+        {
+            var key = new JobKey(jobName, jobGroup);
+            var prevFireTimeUTC = _scheduler.GetTriggersOfJob(key).Result.Max(t => t.GetPreviousFireTimeUtc());
+
+            if(prevFireTimeUTC != null)
+            {
+                var dateTime = prevFireTimeUTC.ToDateTime();
+
+                if(dateTime != null)
+                {
+                    return dateTime.Value.ToLocalTime().Ticks;
+                }
+            }
+            return 0;
+        }
+
+        public static long TriggerJob(string jobGroup, string jobName, JobDataMap map = null)
+        {
+            var job = _scheduler.GetJobDetail(new JobKey(jobName, jobGroup)).Result;
+
+            if(_scheduler.GetCurrentlyExecutingJobs().Result.Any(runningJob => Equals(runningJob.JobDetail, job)))
+            {
+                throw new InvalidOperationException("Job already running");
+            }
+
+            _scheduler.TriggerJob(job.Key, map);
+
+            return DateTime.Now.Ticks;
+        }
+    }
+}
